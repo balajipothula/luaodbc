1e310f20c561abc98faee267a6fd4a21490183b9
BALAJI POTHULA <balan.pothula@gmail.com>
2019-08-11 17:45:51 +0530

    Update lua-odbc.sh

--- lua-odbc.sh
+++ lua-odbc.sh
@@ -1,11 +1,9 @@
 make install CC="gcc -m32" PREFIX=$HOME/luajit
 
-package.cpath = package.cpath .. ";/home/ec2-user/.luarocks/lib/lua/5.1/odbc"
-
-/home/ec2-user/.luarocks/lib/lua/5.1
-
 https://github.com/nmap/nmap
 https://github.com/moteus/lua-odbc
 https://www.programering.com/q/MTM2YzMwATY.html
 https://openresty.org/en/using-luarocks.html
 https://github.com/torch/luajit-rocks
+
+https://www.progress.com/tutorials/odbc/connecting-to-hive-from-lua-using-an-odbc-driver
