3a2657d73f61ff5835d8626311bb57f5853eb7cb
BALAJI POTHULA <balan.pothula@gmail.com>
2019-08-11 18:00:08 +0530

    Update mssqlserver.lua

--- mssqlserver.lua
+++ mssqlserver.lua
@@ -2,5 +2,7 @@ package.path  = package.path  .. ";/home/ec2-user/.luarocks/share/lua/5.1/?.lua"
 package.cpath = package.cpath .. ";/home/ec2-user/.luarocks/lib/lua/5.1/?.so"
 
 local odbc = require("odbc")
-ms_sql_server = odbc:connect ("factset.cgkaelgeb32b.ap-south-1.rds.amazonaws.com:1433", "admin", "admin123")
-print(ms_sql_server)
+odbc.environment():drivers(print)
+
+--ms_sql_server = odbc:connect("tcp://factset.cgkaelgeb32b.ap-south-1.rds.amazonaws.com:1433", "admin", "admin123")
+--ms_sql_server = odbc:connect("tcp://factset.cgkaelgeb32b.ap-south-1.rds.amazonaws.com:1433;user=admin;password=admin123")
