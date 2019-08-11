4f5a7c90ded7687cb28bca71d73060a04aa51f8a
BALAJI POTHULA <balan.pothula@gmail.com>
2019-08-11 15:23:59 +0530

    Update lua.sh

--- lua.sh
+++ lua.sh
@@ -1,5 +1,5 @@
 sudo yum -y update
-sudo yum -y install gcc make readline-devel
+sudo yum -y install gcc make readline-devel unzip
 
 curl http://www.lua.org/ftp/lua-5.1.2.tar.gz -o $HOME/lua.tar.gz && \
 tar -xzf $HOME/lua.tar.gz                                        && \
@@ -7,4 +7,13 @@ rm  -rf  $HOME/lua.tar.gz                                        && \
 mv       $HOME/lua-* $HOME/lua                                   && \
 cd       $HOME/lua                                               && \
 make linux test                                                  && \
+sudo make install
+
+curl -J -L https://luarocks.org/releases/luarocks-3.1.3.tar.gz -o $HOME/luarocks.tar.gz && \
+tar -xzf $HOME/luarocks.tar.gz                                                          && \
+rm  -rf  $HOME/luarocks.tar.gz                                                          && \
+mv       $HOME/luarocks-* $HOME/luarocks                                                && \
+cd       $HOME/luarocks                                                                 && \
+./configure                                                                             && \
+make build                                                                              && \
 sudo make install
