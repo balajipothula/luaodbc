0ffa230774846022d98dd9c92b76a9e095aa30bb
BALAJI POTHULA <balan.pothula@gmail.com>
2019-08-12 16:07:27 +0530

    Update install-luarocks.sh

--- install-luarocks.sh
+++ install-luarocks.sh
@@ -12,7 +12,7 @@ yum -y install gcc git make readline-devel unixODBC-devel unzip
 
 # downloading, extracting and compiling luarocks.
 curl -J -L https://luarocks.org/releases/luarocks-3.1.3.tar.gz -o $HOME/luarocks.tar.gz && \
-tar -xzf $HOME/luarocks.tar.gz                                                          && \
+tar -xzf $HOME/luarocks.tar.gz -C $HOME                                                 && \
 rm  -rf  $HOME/luarocks.tar.gz                                                          && \
 mv       $HOME/luarocks-* $HOME/luarocks                                                && \
 cd       $HOME/luarocks                                                                 && \
