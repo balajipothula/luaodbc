65190c206c4d491fa42090cfd20f7aa30a13ca19
BALAJI POTHULA <balan.pothula@gmail.com>
2019-08-12 15:27:28 +0530

    Update install-luarocks.sh

--- install-luarocks.sh
+++ install-luarocks.sh
@@ -1,6 +1,16 @@
+#!/bin/bash
+
+# Author      : BALAJI POTHULA <balaji.pothula@techie.com>,
+# Date        : 31 August 2016,
+# Description : Installing / Compiling Lua from Source Code.
+
+# update package repositories.
 sudo yum -y update
+
+# installing build essentials.
 sudo yum -y install gcc git make readline-devel unixODBC-devel unzip
 
+# downloading, extracting and compiling luarocks.
 curl -J -L https://luarocks.org/releases/luarocks-3.1.3.tar.gz -o $HOME/luarocks.tar.gz && \
 tar -xzf $HOME/luarocks.tar.gz                                                          && \
 rm  -rf  $HOME/luarocks.tar.gz                                                          && \
