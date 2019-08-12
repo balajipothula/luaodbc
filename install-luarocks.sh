ad2b66ccd4100233d51ad817a29fe4ce00ef1aa9
BALAJI POTHULA <balan.pothula@gmail.com>
2019-08-12 11:51:52 +0530

    Update install-luarocks.sh

--- install-luarocks.sh
+++ install-luarocks.sh
@@ -1,19 +1,6 @@
 sudo yum -y update
 sudo yum -y install gcc git make readline-devel unixODBC-devel unzip
 
-sleep 5
-
-curl http://www.lua.org/ftp/lua-5.1.2.tar.gz -o $HOME/lua.tar.gz && \
-tar -xzf $HOME/lua.tar.gz                                        && \
-rm  -rf  $HOME/lua.tar.gz                                        && \
-mv       $HOME/lua-* $HOME/lua                                   && \
-cd       $HOME/lua                                               && \
-make linux test                                                  && \
-sudo make install                                                && \
-rm  -rf  $HOME/lua
-
-sleep 5
-
 curl -J -L https://luarocks.org/releases/luarocks-3.1.3.tar.gz -o $HOME/luarocks.tar.gz && \
 tar -xzf $HOME/luarocks.tar.gz                                                          && \
 rm  -rf  $HOME/luarocks.tar.gz                                                          && \
@@ -23,7 +10,3 @@ cd       $HOME/luarocks                                                         
 make build                                                                              && \
 sudo make install                                                                       && \
 rm  -rf  $HOME/luarocks
-
-sleep 5
-
-luarocks install --local odbc
