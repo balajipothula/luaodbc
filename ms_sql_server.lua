0a45deadc8643ff914d2a2d4e068a20db027cd38
BALAJI POTHULA <balan.pothula@gmail.com>
2019-08-12 13:04:58 +0530

    Update and rename test.lua to ms_sql_server.lua

---
+++ ms_sql_server.lua
@@ -0,0 +1,28 @@
+--> getting home path.
+local HOME = os.getenv("HOME")
+
+--> setting odbc shared object path.
+package.cpath = HOME .. "/.luarocks/lib/lua/5.1/?.so;;"    .. package.cpath
+
+--> setting odbc lua library path.
+package.path  = HOME .. "/.luarocks/share/lua/5.1/?.lua;;" .. package.path
+
+--> initializing odbc library.
+local odbc = require("odbc")
+
+--> initializing odbc environment.
+local env, err = odbc.environment()
+
+--> microsoft sql server connection table.
+local con_table = {
+                    { Driver   = "ODBC Driver 17 for SQL Server" },
+                    { Server   = "factset.cgkaelgeb32b.ap-south-1.rds.amazonaws.com,1433" },
+                    { Uid      = "admin" },
+                    { Pwd      = "admin123" },
+                    { Database = "master" },
+                  }
+
+--> getting microsoft sql server connection.
+local con, err = env:driverconnect(con_table)
+
+print(con)
