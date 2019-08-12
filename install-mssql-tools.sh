7f70acfb9b2a1f2b5fc3581ca3b9eaba48804dc0
BALAJI POTHULA <balan.pothula@gmail.com>
2019-08-12 15:45:19 +0530

    Update install-mssql-tools.sh

--- install-mssql-tools.sh
+++ install-mssql-tools.sh
@@ -1,13 +1,34 @@
-sudo su
-curl https://packages.microsoft.com/config/rhel/7/prod.repo > /etc/yum.repos.d/msprod.repo
-exit
+#!/bin/bash
 
-sudo yum -y remove mssql-tools unixODBC-utf16-devel
-sudo yum -y install mssql-tools unixODBC-devel
+# Author      : BALAJI POTHULA <balaji.pothula@techie.com>,
+# Date        : 12 August 2019,
+# Description : Installing MS Tools.
 
-sudo yum -y check-update
-sudo yum -y update mssql-tools
+# update package repositories.
+yum -y update
 
+# adding mssql-tools repository on rhel_v8.
+cat > /etc/yum.repos.d/msprod.repo <<EOF
+[packages-microsoft-com-prod]
+name=packages-microsoft-com-prod
+baseurl=https://packages.microsoft.com/rhel/8/prod/
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc
+EOF
+
+# removing previously installed mssql-tools and unixODBC-utf16-devel.
+yum -y remove  mssql-tools unixODBC-utf16-devel
+
+# installing mssql-tools and unixODBC-devel.
+yum -y install mssql-tools unixODBC-devel
+
+# updating mssql-tools to latest.
+yum -y update mssql-tools
+
+# setting mssql-tools binaries path permanently.
 echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bash_profile
 echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
+
+# updating environment path.
 source ~/.bashrc
