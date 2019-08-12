113993457da86128950e172b29718857ba7946a5
BALAJI POTHULA <balan.pothula@gmail.com>
2019-08-12 15:46:17 +0530

    Update install-lua.sh

--- install-lua.sh
+++ install-lua.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Author      : BALAJI POTHULA <balaji.pothula@techie.com>,
-# Date        : 31 August 2016,
+# Date        : 12 August 2019,
 # Description : Installing / Compiling Lua from Source Code.
 
 # update package repositories.
