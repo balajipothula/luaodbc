6101eb149122172cd568521883f62ef033c02f2a
BALAJI POTHULA <balan.pothula@gmail.com>
2019-08-12 17:24:07 +0530

    Create contable.lua

---
+++ contable.lua
@@ -0,0 +1,6 @@
+return { { Driver   = "ODBC Driver 17 for SQL Server" },
+         { Server   = "factset.cgkaelgeb32b.ap-south-1.rds.amazonaws.com,1433" },
+         { Uid      = "admin" },
+         { Pwd      = "admin123" },
+         { Database = "office_db" },
+}
