3241d9937f3c9dfe1dbd2db69d766b789d4245f9
BALAJI POTHULA <balan.pothula@gmail.com>
2019-08-12 12:00:12 +0530

    Update mssql-tools.sh

--- mssql-tools.sh
+++ mssql-tools.sh
@@ -1,20 +1,4 @@
-sudo su
-curl https://packages.microsoft.com/config/rhel/7/prod.repo > /etc/yum.repos.d/msprod.repo
-exit
-sudo yum remove mssql-tools unixODBC-utf16-devel
-sudo yum install mssql-tools unixODBC-devel
-
-sudo yum check-update
-sudo yum update mssql-tools
-
-echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bash_profile
-echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
-source ~/.bashrc
-
 sqlcmd -S tcp:factset.cgkaelgeb32b.ap-south-1.rds.amazonaws.com,1433 -U admin -P admin123
 
 select DB_NAME()
 GO
-
-
-https://docs.microsoft.com/en-us/sql/connect/odbc/linux-mac/installing-the-microsoft-odbc-driver-for-sql-server?view=sql-server-2017
