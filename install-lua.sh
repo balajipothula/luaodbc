724fa4fb26695cac9d22b2a928a8677914ec646f
BALAJI POTHULA <balan.pothula@gmail.com>
2019-08-12 15:25:53 +0530

    Update install-lua.sh

--- install-lua.sh
+++ install-lua.sh
@@ -1,6 +1,16 @@
+#!/bin/bash
+
+# Author      : BALAJI POTHULA <balaji.pothula@techie.com>,
+# Date        : 31 August 2016,
+# Description : Installing / Compiling Lua from Source Code.
+
+# update package repositories.
 sudo yum -y update
+
+# installing build essentials.
 sudo yum -y install gcc git make readline-devel unixODBC-devel unzip
 
+# downloading, extracting and compiling lua.
 curl http://www.lua.org/ftp/lua-5.1.2.tar.gz -o $HOME/lua.tar.gz && \
 tar -xzf $HOME/lua.tar.gz                                        && \
 rm  -rf  $HOME/lua.tar.gz                                        && \
