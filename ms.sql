e6061aa65851a591df7b68fe39d4c2bbeead4fb0
BALAJI POTHULA <balan.pothula@gmail.com>
2019-08-13 14:40:07 +0530

    Update ms.sql

--- ms.sql
+++ ms.sql
@@ -1,11 +1,11 @@
 -- creating database.
-CREATE DATABASE IF NOT EXIST office_db
+CREATE DATABASE IF NOT EXISTS office_db
 
 -- using database.
 USE office_db
 
 -- creating table.
-CREATE TABLE IF NOT EXIST emp_tab(no INT, name VARCHAR(10))
+CREATE TABLE IF NOT EXISTS emp_tab(no INT, name VARCHAR(10))
 
 -- inserting data / records into table.
 INSERT INTO emp_tab(no, name) VALUES(1001, 'Ram')
