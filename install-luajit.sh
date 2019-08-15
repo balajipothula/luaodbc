74deacb64c1f2e545572fc89a98d39fb5d540100
BALAJI POTHULA <balan.pothula@gmail.com>
2019-08-15 18:04:19 +0530

    Update install-luajit.sh

--- install-luajit.sh
+++ install-luajit.sh
@@ -11,11 +11,11 @@ sudo yum -y update
 sudo yum -y install gcc make
 
 # downloading, extracting and compiling lua.
+mkdir -p $HOME/luajit                                                      && \
 curl http://luajit.org/download/LuaJIT-2.0.5.tar.gz -o $HOME/LuaJIT.tar.gz && \
 tar -xzf $HOME/LuaJIT.tar.gz -C $HOME                                      && \
 rm  -rf  $HOME/LuaJIT.tar.gz                                               && \
 mv       $HOME/LuaJIT-* $HOME/LuaJIT                                       && \
 cd       $HOME/LuaJIT                                                      && \
-sudo make install                                                          && \ : '
-make install PREFIX=$HOME/luajit                                           && \   '
+make install CC="gcc -m32" TARGET_SYS=Linux PREFIX=$HOME/luajit            && \
 rm  -rf  $HOME/LuaJIT
